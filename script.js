fbb0e293fab5a6138523cacac49586c664212df8
Wmtmky <75718016+Wmtmky@users.noreply.github.com>
2022-01-02 23:26:14 -0800

    PHP Test

--- script.js
+++ script.js
@@ -13,24 +13,6 @@ window.onresize = function () {
 
 };
 
-/*
-let oldScroll = 0;
-window.addEventListener("scroll", function() {
-    let scroll = window.scrollY;
-    let height = window.innerHeight;
-
-    if (scroll < 0) {
-        window.scrollY = 0;
-        console.log("0");
-    } else if (oldScroll < scroll && scroll < height) {
-        window.location.replace("#projects");
-        console.log("projects");
-    }
-
-    oldScroll = scroll;
-});
-*/
-
 function getTheme() {
 
     let lightMode = localStorage.getItem('--lightMode');
@@ -71,18 +53,39 @@ function setTheme(value, property) {
 
 }
 
+let focusX;
+let focusY;
+let dynBgInterval = setInterval(function(){});
+document.onmousemove = function(e) {
+    clearInterval(dynBgInterval);
+    dynamicBackground(e);
+    dynBgInterval = setInterval(function(){
+        dynamicBackground(e);
+    }, 8);
+};
 function dynamicBackground(event) {
     let grad = getComputedStyle(document.documentElement).getPropertyValue('--bgMain');
     if (grad == null) return;
+    if ((focusX > event.clientX - 10 && focusX < event.clientX + 10) || (focusY > event.clientY - 10 && focusY < event.clientY + 10)) return;
 
     // rotate
-    let speed = Math.random() / 2 // deg per mousemove
+    let change = 0.5 // deg per mousemove
     let deg = grad.split(/deg/)[0].split(" ");
-    deg = (parseFloat(deg[deg.length - 1]) + speed) % 360;
+    deg = (parseFloat(deg[deg.length - 1]) + (change * 0.25)) % 360;
 
-    let xPos = event.clientX / window.innerWidth * 100;
-    let yPos = event.clientY / window.innerHeight * 100;
-    
+    if (focusX == null || focusY == null) {
+        focusX = window.innerWidth * 0.5;
+        focusY = window.innerHeight * 0.5;
+    }
+
+    let xPos = (focusX + (event.clientX - focusX) * 0.01 * change);
+    let yPos = (focusY + (event.clientY - focusY) * 0.01 * change);
+
+    focusX = xPos;
+    focusY = yPos;
+    xPos = xPos / window.innerWidth * 100;
+    yPos = yPos / window.innerHeight * 100;
+
     grad = "radial-gradient(farthest-corner at " + xPos + "% " + yPos + "%, #DDDF, #EEED 10%, #EEE9 30%, #FFF4 60%, #FFF1 80%), conic-gradient(from " + deg + "deg at " + xPos + "% " + yPos + "%, #F28, #71C, #52B, #46F, #49F, #5DF, #5BF, #99F, #D6D, #F28)";
 
     setTheme('--bgMain', grad);
